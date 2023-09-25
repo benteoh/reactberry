3a444d2f61808661e3e0a0633eaac76561fb0c75
benben175 <benben1752002@gmail.com>
2023-09-25 20:00:56 +0100

    Created demos for callback and non-callback

--- AudioGraphDemo.py
+++ AudioGraphDemo.py
@@ -1,6 +1,6 @@
 '''
 This is a simple demo that utilises the AudioDriver class, plotting the audio
-data sourced from the microphone in real time.
+data sourced from the microphone in real time, using the listen_and_do function.
 '''
 import AudioDriver as AD
 import AudioHandler as AH
@@ -8,12 +8,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Some constants
-CHUNKS = 20         # Number of chunks to plot (reduce for better performance)
+CHUNKS = 10         # Number of chunks to plot (reduce for better performance)
 CHUNK_SIZE = 1024   # Number of samples per chunk
+RATE = 44100        # Sampling rate
 
 def main():
-    # Initialize audio driver and handler
-    audio_handler = AH.AudioHandler(AD.AudioDriver())
+    # Initialize audio driver
+    audio_driver = AD.AudioDriver()
+    audio_driver.open_stream()
     
     # Set up plot
     plt.ion()
@@ -21,17 +23,21 @@ def main():
     x = np.arange(0, CHUNK_SIZE * CHUNKS)
     line, = ax.plot(x, np.random.rand(CHUNK_SIZE * CHUNKS))
     ax.set_ylim(-30000, 30000)
-
-    # Grab snapshots of audio and plot them.
-    while (audio_handler.is_active()):
+    
+    # Set up aux function for listen_and_do
+    def aux(audio):
         try:
-            line.set_ydata(audio_handler.listen().get_n_chunks(CHUNKS))
-            fig.canvas.flush_events()
+            if (audio.audio_array.size >= CHUNK_SIZE * CHUNKS):
+                line.set_ydata(audio.get_n_chunks(CHUNKS))
+                fig.canvas.flush_events()
+            return True
         except:
-            break
+            return False
+
+    audio_driver.listen_and_do(1, aux)
     
     # Clean up
-    audio_handler.close()
+    audio_driver.close_stream()
     plt.close(fig)
         
 if __name__ == "__main__":
