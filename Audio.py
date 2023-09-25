a3457f5d49f31dbbac8ab56fa289f0ada81c8a1c
benben175 <benben1752002@gmail.com>
2023-09-25 19:59:57 +0100

    Added bounds safety to functions

--- Audio.py
+++ Audio.py
@@ -11,10 +11,16 @@ class Audio:
         return self.audio_array.size / self.rate
     
     def get_n_seconds(self, n) -> np.array:
+        if self.audio_array.size < n * self.rate:
+            return self.audio_array
         return self.audio_array[-n * self.rate:]
     
     def get_latest_chunk(self) -> np.array:
+        if self.audio_array.size == 0:
+            return None
         return self.audio_array[-self.chunk_size:]
     
     def get_n_chunks(self, n) -> np.array:
+        if self.audio_array.size < n * self.chunk_size:
+            return self.audio_array
         return self.audio_array[-n * self.chunk_size:]
