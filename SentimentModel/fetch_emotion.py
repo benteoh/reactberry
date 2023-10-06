1eb8afef14a28475f7a7eb2c9369484bb59a6381
benteoh <benben1752002@gmail.com>
2023-10-06 16:08:42 +0000

    Analysed wrong predictions

--- SentimentModel/fetch_emotion.py
+++ SentimentModel/fetch_emotion.py
@@ -5,3 +5,9 @@ def get_emotion_number(file_path):
     if code == '01' or code == '02':
         return 0;
     return int(code) - 2
+
+# Returns the corresponding emotion in string form. 
+def get_emotion_str(n):
+    emotions = ['neutral', 'happy', 'sad', 'angry', 'fearful', 'digust', 'surprised']
+    return emotions[n]
+    
