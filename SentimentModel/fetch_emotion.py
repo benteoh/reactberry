9f1841bc16de4cc6a12077764cb4c66b2972e678
benteoh <benben1752002@gmail.com>
2023-10-10 12:27:55 +0000

    Added TESS dataset and format into RAVDESS style

--- SentimentModel/fetch_emotion.py
+++ SentimentModel/fetch_emotion.py
@@ -1,7 +1,7 @@
 # Returns the corresponding emotion number from 0 to 6. Note that calm and neutral are combined to code 0.
 def get_emotion_number(file_path):
     item = file_path.split('/')[-1]
-    code = item[6:-16]
+    code = item.split('-')[2]
     if code == '01' or code == '02':
         return 0;
     return int(code) - 2
